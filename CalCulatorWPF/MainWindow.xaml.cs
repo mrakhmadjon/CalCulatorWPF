d9920d2721ede7c00dc8f3c2d4b5d45d927cc2a7
HP <HP@LAPTOP-AK9U0JDU>
2022-02-18 17:00:39 +0500

    some kinda finished

--- CalCulatorWPF/MainWindow.xaml.cs
+++ CalCulatorWPF/MainWindow.xaml.cs
@@ -24,5 +24,136 @@ namespace CalCulatorWPF
         {
             InitializeComponent();
         }
+
+        private double num = 0;
+        private double num2 = 0;
+        private string opera;
+        private void Digit_Click(object sender, RoutedEventArgs e)
+        {
+            Button n = (Button)sender;
+            if (inp.Text == "Error")
+                inp.Text = "";
+            inp.Text += n.Content.ToString();
+        }
+        private void clear(object sender, RoutedEventArgs e)
+        {
+            inp.Text = "";
+        }
+
+        private void Dell(object sender, RoutedEventArgs e)
+        {
+            if(inp.Text.Length != 0)
+                inp.Text = inp.Text.Substring(0, inp.Text.Length - 1);
+        }
+
+       
+
+        private void Equal(object sender, RoutedEventArgs e)
+        {
+            string [] a = inp.Text.Split('+', '-','/','x','^');
+            if(opera == "+")
+                num = double.Parse(a[0]) + double.Parse(a[1]); 
+            else if (opera == "-")
+                num = double.Parse(a[0]) - double.Parse(a[1]);
+            else if (opera == "x")
+                num = double.Parse(a[0]) * double.Parse(a[1]);
+            else if (opera == "/")
+                num = double.Parse(a[0]) / double.Parse(a[1]);
+            else
+                num = Math.Pow(double.Parse(a[0]),double.Parse(a[1]));
+
+            inp.Text = num.ToString();
+        }
+
+        private void Point(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(inp.Text) && !inp.Text.Contains("."))
+                inp.Text += ".";
+
+            if (inp.Text.Contains("+") || inp.Text.Contains("-") || inp.Text.Contains("x") || inp.Text.Contains("/") || inp.Text.Contains("^"))
+            {
+                string[] nums = inp.Text.Split('+', '-', '/', 'x', '^');
+                if ( !nums[1].Contains("."))
+                    inp.Text += ".";
+            }
+           
+
+
+        }
+
+        
+
+        private void Sqrt(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double res = Math.Sqrt(double.Parse(inp.Text));
+
+                inp.Text = res.ToString();
+            }
+            catch
+            {
+
+                inp.Text = "Error";
+            }
+          
+        }
+
+
+        private void Operation(object sender, RoutedEventArgs e)
+        {
+
+            Button btn = (Button)sender;
+            string? operation = btn.Content.ToString();
+            if (!string.IsNullOrEmpty(inp.Text) && inp.Text != "Error")
+            {
+                if (operation == "sqr")
+                {
+                    if (!inp.Text.Contains("x") && !inp.Text.Contains("-") && !inp.Text.Contains("+") && !inp.Text.Contains("/") && !inp.Text.Contains("^"))
+                    {
+                        inp.Text += "^";
+                        opera = "^";
+                    }
+                }
+
+                else if (operation == "x")
+                {
+                    if (!inp.Text.Contains("^") && !inp.Text.Contains("-") && !inp.Text.Contains("+") && !inp.Text.Contains("/") && !inp.Text.Contains("x"))
+                    {
+                        inp.Text += "x";
+                        opera = "x";
+                    }
+                }
+
+                else if (operation == "/")
+                {
+                    if (!inp.Text.Contains("x") && !inp.Text.Contains("-") && !inp.Text.Contains("+") && !inp.Text.Contains("^") && !inp.Text.Contains("/"))
+                    {
+                        inp.Text += "/";
+                        opera = "/";
+                    }
+                }
+                else if (operation == "-")
+                {
+                    if (!inp.Text.Contains("x") && !inp.Text.Contains("^") && !inp.Text.Contains("+") && !inp.Text.Contains("/") && !inp.Text.Contains("-"))
+                    {
+                        inp.Text += "-";
+                        opera = "-";
+                    }
+                }
+
+                else
+                {
+                    if (!inp.Text.Contains("x") && !inp.Text.Contains("-") && !inp.Text.Contains("^") && !inp.Text.Contains("/") && !inp.Text.Contains("+"))
+                    {
+                        inp.Text += "+";
+                        opera = "+";
+                    }
+                }
+
+
+            }
+
+        }
     }
 }
